7ca7bba7af5c48af1ee5f9d9bb8f972672261d06
jaeinkim <top100itw@nate.com>
2020-04-14 14:00:55 +0900

    delete

--- MPMS_TEST/poms/jobs/process/ods_project_0.1/src/main/templates/Job_run_template.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/mysql-connector-java-8.0.18.jar;../lib/postgresql-42.2.9.jar;../lib/crypto-utils.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;ods_project_0_1.jar;' mpms.ods_project_0_1.ODS_PROJECT  --context=Default $args
